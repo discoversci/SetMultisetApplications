0cb3c24982bf2af5dac7148bacafb960e1df2f05
Janez Perme <discoversci@hotmail.com>
2020-01-07 07:52:58 +0100

    nove metode

--- SetMultisetApplication02/SetMultisetApplication02.cpp
+++ SetMultisetApplication02/SetMultisetApplication02.cpp
@@ -1,20 +1,46 @@
-// SetMultisetApplication02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// constructor and assignment operator – multiset
+#include <set>
 #include <iostream>
+#include <functional>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+template <class T> void print(T start, T end) {
+	while (start != end) {
+		std::cout << *start << " "; start++;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	int t[] = { 2, 10, 8, 4, 5, 5, 3, 10, 7, 2 };
+
+	//1. standard initialization - default constructor
+	multiset <int> s1;
+
+	//2. iterator constructor
+	multiset <int> s2(t, t + 10);
+	print(s2.begin(), s2.end()); cout << endl;
+
+	//3. copy constructor
+	multiset <int> s3(s2);
+	print(s3.begin(), s3.end()); cout << endl;
+
+	//4. providing different comparator
+	multiset<int, greater <int> > s4(t, t + 10);
+	print(s4.begin(), s4.end()); cout << endl;
+
+	//5. Not allowed - source and target object are not of the same type
+	//podobno kot pri prejsnjem primeru - nekompatibilen comparator
+	//multi_set<int, greater<int> > s5 (s3);
+
+	//6. assignment
+	multiset <int> s6;
+	s6 = s3;
+	print(s6.begin(), s6.end()); cout << endl;
+
+	//7. Not allowed - assignment of incompatible multi_set objects
+	//podobno kot pri prejsnjem primeru - nekompatibilen comparator
+	//s6 = s4;
+	return 0;
+}
